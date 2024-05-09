6a9bacf492198b67120985d932faa6bde894aaf2
Taylor <4.alfred.taylor@gmail.com>
2024-05-09 13:38:30 -0400

    commit

---
+++ demos/website.js
@@ -0,0 +1,77 @@
+"use strict";
+
+
+const usStatesAndTerritories = [
+    "Alabama",
+    "Alaska",
+    "American Samoa",
+    "Arizona",
+    "Arkansas",
+    "California",
+    "Colorado",
+    "Connecticut",
+    "Delaware",
+    "District of Columbia",
+    "Florida",
+    "Georgia",
+    "Guam",
+    "Hawaii",
+    "Idaho",
+    "Illinois",
+    "Indiana",
+    "Iowa",
+    "Kansas",
+    "Kentucky",
+    "Louisiana",
+    "Maine",
+    "Maryland",
+    "Massachusetts",
+    "Michigan",
+    "Minnesota",
+    "Mississippi",
+    "Missouri",
+    "Montana",
+    "Nebraska",
+    "Nevada",
+    "New Hampshire",
+    "New Jersey",
+    "New Mexico",
+    "New York",
+    "North Carolina",
+    "North Dakota",
+    "Northern Mariana Islands",
+    "Ohio",
+    "Oklahoma",
+    "Oregon",
+    "Pennsylvania",
+    "Puerto Rico",
+    "Rhode Island",
+    "South Carolina",
+    "South Dakota",
+    "Tennessee",
+    "Texas",
+    "U.S. Virgin Islands",
+    "Utah",
+    "Vermont",
+    "Virginia",
+    "Washington",
+    "West Virginia",
+    "Wisconsin",
+    "Wyoming"
+];
+
+const statesList = document.getElementById("statesList");
+
+window.onload = function(){
+    populateStatesList();
+};
+
+function populateStatesList(){
+console.log("populate");
+
+for(;et i = 0; i < usStatesAndTerritories.length; i++){
+    let newOption = document.createElement("option");
+    newOption.textContent = usStatesAndTerritories[i];
+    statesList.appendChild(newOption);
+}
+}
